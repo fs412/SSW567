c0176370266b6b076d81e7fa8091750f21e6e8e5
fs412 <49499662+fs412@users.noreply.github.com>
2019-09-29 18:27:44 -0400

    Add files via upload

--- HW05FS.py
+++ HW05FS.py
@@ -1,5 +1,5 @@
 """ HW 05: Testing triangle classification """
-import math
+import unittest
 
 def main():
     """ Input from the user and displaying any errors from the inputs.""" 
@@ -49,6 +49,21 @@ def classify_triangle(a, b, c):
     elif (a != b != c):
         return("This is a scalene triangle.")
     
+class classify_triangle_test(unittest.TestCase):
+    """ Test for classifying triangles."""
+    def test_triangle(self):
+        self.assertEqual(classify_triangle(3, 3, 3), "This is an equilateral triangle.")
+        self.assertEqual(classify_triangle(8, 8, 8), "This is an equilateral triangle.")
+        self.assertEqual(classify_triangle(5, 5, 9), "This is an isosceles triangle.")
+        self.assertEqual(classify_triangle(6, 6, 9), "This is an isosceles triangle.")
+        self.assertEqual(classify_triangle(5, 4, 3), "This is a right, scalene triangle.")
+        self.assertEqual(classify_triangle(4, 5, 8), "This is a scalene triangle.")
+        self.assertEqual(classify_triangle(6, 7, 8), "This is a scalene triangle.")
+        self.assertEqual(classify_triangle(8, 8, 9), "This is an isosceles triangle.")
+        self.assertEqual(classify_triangle(11.313708498984,11.313708498984,16), "This is a right, isosceles triangle.")
+        self.assertEqual(classify_triangle(3, 3, 9), "This is not a triangle.")
+        self.assertNotEqual(classify_triangle(3, 3, 3), "This is not a triangle.")
+
 if __name__ == '__main__':
-   main()
+   unittest.main(exit=False, verbosity=2)
     
